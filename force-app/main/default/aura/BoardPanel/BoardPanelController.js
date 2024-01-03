719c48c63d49d25a9298ebec80d744f50b5653ee
Rohith3002 <rohith.3002@gmail.com>
2024-01-03 14:33:43 +0530

    Hiding

--- force-app/main/default/aura/BoardPanel/BoardPanelController.js
+++ force-app/main/default/aura/BoardPanel/BoardPanelController.js
@@ -1,17 +1,13 @@
 ({
     startGame: function (component, event, helper) {
-        /* eslint-disable no-console */
         // access combobox
         let gameModeComboBox = component.find("gameMode");
 
         // access the value of combobox
         let selectedValue = gameModeComboBox.get("v.value");
 
-        //update selectedMode attribute
+        // update selectedMode attribute
         component.set("v.selectedMode", selectedValue);
-
-        console.log("The start new game button is clicked. The game mode is " + selectedValue);
-        console.log("Selected mode attribute value: " + component.get("v.selectedMode"));
     },
 
     reshuffleBoard: function (component, event, helper) {
